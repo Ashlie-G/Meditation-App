251260e009c9322222f2670cb9dfe2d7c5c52f6b
Ashlie Gray <ashliejanegray@gmail.com>
2021-01-06 21:45:30 +1000

    animation added to progress bar round play/pause button

--- app.js
+++ app.js
@@ -34,6 +34,17 @@ const app = () => {
     }
 
 }
+//animate circle and check time
+song.ontimeupdate = () => {
+    let currentTime = song.currentTime
+    let elapsed = fakeDuration - currentTime
+    let seconds = Math.floor(elapsed % 60)
+    let minutes = Math.floor(elapsed / 60)
+
+    //progress bar animation
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength
+    outline.style.strokeDashoffset = progress
+}
 }
 
 app()
